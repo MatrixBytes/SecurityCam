f752eca753f7eaa1a4b85e23a8487799778f045d
MatrixBytes <89941741+MatrixBytes@users.noreply.github.com>
2021-11-02 11:36:21 +0100

    Add files via upload

---
+++ script.py
@@ -0,0 +1,17 @@
+import index as script
+
+script.clear()
+
+script.start()
+
+print(script.log('Hello World'))
+
+script.sleep(3)
+
+print(script.userinp('Write something'))
+
+print(script.randomnum('50', '100'))
+
+print(script.randomstr(['Pepo', 'Max', 'Hey']))
+
+script.stop()
