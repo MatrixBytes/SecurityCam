321d5dd874eaa3f6b8621cc2e2224594d13232b9
Neo <89941741+MatrixBytes@users.noreply.github.com>
2022-03-20 19:26:06 +0100

    Update webcam.py

--- webcam.py
+++ webcam.py
@@ -1,29 +1,36 @@
 import cv2
-import datetime
-import threading
+from datetime import datetime
+from time import sleep
+from os import path
 
 capture = cv2.VideoCapture(0)
 faces = cv2.CascadeClassifier("detect.xml")
 
+semi_fps = 0
+
 while True:
-    text = 'Time ' + datetime.datetime.now().strftime("%d-%m-%y ~ %H:%M:%S")
+    time = datetime.now().strftime("%d-%m-%y %H:%M:%S")
+    text = 'Time ' + time
     ret, frame = capture.read()
 
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     detections = faces.detectMultiScale(frame_gray, scaleFactor=1.1, minNeighbors=6)
 
-    for (x, y, w, h) in detections:
-        cv2.line(frame, (x, y), (0, 0), (255, 0, 0))
-        cv2.line(frame, (x, y), (640, 480), (0, 255, 0))
-        cv2.line(frame, (x, y), (640, 0), (0, 0, 255))
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 255, 255), 1)
-        cv2.putText(frame, 'Person found!', (230, 35), cv2.FONT_HERSHEY_TRIPLEX, 1.0, (255, 255, 255), thickness = 1)
+    if detections != ():
+        for (x, y, w, h) in detections:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 255, 255), 1)
 
-    cv2.putText(frame, text, (10, 465), cv2.FONT_HERSHEY_TRIPLEX, 0.65, (255, 255, 255), thickness = 1)
-    cv2.imshow('Webcam', frame)
+        filename = f'Person: {time}.jpg'
+        print(f'Person found! [{time}] [{filename}]')
+        cv2.imwrite(path.join('records', filename), frame)
+        semi_fps = 1
+    else:
+        semi_fps = 0
 
     if cv2.waitKey(1) == ord('q'):
         break
+    
+    sleep(semi_fps)
 
 capture.release()
 cv2.destroyAllWindows()
